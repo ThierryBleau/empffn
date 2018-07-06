8d02b4de40838ddbde8bfe1f19203c8ab237fe65
Thierry Bleau <thierryb1996@gmail.com>
2018-07-05 23:09:19 -0400

    trouble importing ffn

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ import os
 
 
 def main():
-    print("hello world")
+    returns = ffn.get('aapl,msft,c,gs,ge', start='2010-01-01').to_returns().dropna()
+    returns.calc_mean_var_weights().as_format('.2%')
+    print(returns)
+
+    returns2 = np.array([.01, .02, .03, -.4, -.06, -.02])
+    benchmark_returns = np.array([.02, .02, .03, -.35, -.05, -.01])
+
+    emp.max_drawdown(returns2)
+
+    alpha, beta = emp.alpha_beta(returns2, benchmark_returns)
+    print(alpha)
+    print(beta)
 
 main()

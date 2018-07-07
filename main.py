576d6d7f98e0c7114607706a89d9a9f10ab580ad
Thierry Bleau <thierryb1996@gmail.com>
2018-07-06 21:16:41 -0400

    done tail_ratio function

--- main.py
+++ main.py
@@ -7,17 +7,33 @@ import os
 
 
 def main():
-    returns = ffn.get('aapl,msft,c,gs,ge', start='2010-01-01').to_returns().dropna()
-    returns.calc_mean_var_weights().as_format('.2%')
-    print(returns)
+    #returns = ffn.get('aapl,msft,c,gs,ge', start='2010-01-01').to_returns().dropna()
+    #returns.calc_mean_var_weights().as_format('.2%')
+    #print(returns)
 
     returns2 = np.array([.01, .02, .03, -.4, -.06, -.02])
     benchmark_returns = np.array([.02, .02, .03, -.35, -.05, -.01])
 
     emp.max_drawdown(returns2)
 
+    dates = pd.date_range('20130101',periods = 6)
+    df = pd.DataFrame(np.random.randn(6,4),index=dates,columns=list('ABCD'))
+
     alpha, beta = emp.alpha_beta(returns2, benchmark_returns)
+    tratios = va_tail_ratio(df)
+    print(tratios)
     print(alpha)
     print(beta)
+
+def get_data(filename):
+    # read dataframe from file?
+    pass
+
+def va_tail_ratio(returns):
+    ratios = []
+    for ticker in returns:
+        ratios.append(emp.tail_ratio(returns[ticker]))
+    return ratios
+
 
 main()
